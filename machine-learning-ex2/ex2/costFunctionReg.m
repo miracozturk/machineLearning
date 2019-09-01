77fdb591e6425d20b778d21b06a7df9a75f68eee
ozturkmi <no_email@gmail.com>
2019-09-01 16:38:39 +0300

    Coursera machine learning exercise 2 solution completed.

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,11 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+ hx = sigmoid(X * theta); % matrix multiplication
+ regression = (lambda/(2*m))*(sum(theta(2:end).^2))
+ J = -1/m*((y' * log(hx)) + (1-y)'*log(1-hx));
+ J = J + regression;
 
-
-
-
-
+ grad(1) =     (1/m)*(hx - y)'*X(:,1); 
+ grad(2:end) = ((1/m)*(hx - y)'*X(:, 2:end))' + (lambda/m)*theta(2:end);
 % =============================================================
-
 end
